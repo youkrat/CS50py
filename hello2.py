0f621196e1534dbbfabd25ef60b3ec28e2adb635
Ben <bmwangi676@gmail.com>
2025-05-28 00:31:13 +0300

    Defining main def main():

--- hello2.py
+++ hello2.py
@@ -1,11 +1,14 @@
+#Defining the main part of the code 
+def main():
+#Removing whitespace and capitalizing the user's name
+    name = input("What's your name? ",).strip().title()
+    first, second, surname = name.split()
+#Calling the function hello, and saying hello to the user
+    hello(name)
+
 #Defining the Hello function and the parameters
 #(to="world") sets the default value for the parameter to 
 def hello(to="world"):
     print("Hello,", to )
-#Calls hello without the name. Therefore; Hello world 
-hello()
-#Removing whitespace and capitalizing the user's name
-name = input("What's your name? ",).strip().title()
-first, second, surname = name.split()
-#Calling the function hello, and saying hello to the user
-hello(name)
+#Calling the main function
+main()
