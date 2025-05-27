f27dbd92fc476cea4af08194238f7c5d59d0e4db
Ben <bmwangi676@gmail.com>
2025-05-28 00:11:47 +0300

    Added comments

--- hello2.py
+++ hello2.py
@@ -1,8 +1,10 @@
+#Defining the Hello function
 def hello():
     print("Hello", end="", sep="")
-
+#Removing whitespace and capitalizeing the user's name
 name = input("What's your name? ",).strip().title()
 first, second, surname = name.split()
+#Calling the function hello, and saying hello to the user
 hello()
 print(f" {first}")
 print(f"Is your full name, {first} {second} {surname}?")
