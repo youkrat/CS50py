9beaf33dc496b08641d31260c8595890840a2d33
Ben <bmwangi676@gmail.com>
2025-05-28 00:26:28 +0300

    Defining parameters

--- hello2.py
+++ hello2.py
@@ -1,10 +1,11 @@
-#Defining the Hello function
-def hello():
-    print("Hello", end="", sep="")
-#Removing whitespace and capitalizeing the user's name
+#Defining the Hello function and the parameters
+#(to="world") sets the default value for the parameter to 
+def hello(to="world"):
+    print("Hello,", to )
+#Calls hello without the name. Therefore; Hello world 
+hello()
+#Removing whitespace and capitalizing the user's name
 name = input("What's your name? ",).strip().title()
 first, second, surname = name.split()
 #Calling the function hello, and saying hello to the user
-hello()
-print(f" {first}")
-print(f"Is your full name, {first} {second} {surname}?")
+hello(name)
