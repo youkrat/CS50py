d59d1124dd53be3dc0f8393aa24b0f3e414c292a
Ben <bmwangi676@gmail.com>
2025-05-27 12:36:02 +0300

    Splits username

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 #Asks user for their name, removes whitespace and capitalizes user's name
 name = input("What's your name? ").strip().title()
 
+#Splits user's name into first name last name and surname 
+first, second, last = name.split(" ")
 #Say hello to the user
-print(f"Hello,{name} ",end="",sep=" ")
+print(f"Hello,{first} ",end="",sep=" ")
+print (f"Is your full name,{first} {second} {last}?")
