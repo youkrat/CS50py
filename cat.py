2d1d5f326cea90cb78bfa194e804b1017c3a8949
Ben <bmwangi676@gmail.com>
2025-06-03 13:11:21 +0300

    Define meow

--- cat.py
+++ cat.py
@@ -1,7 +1,14 @@
-while True:
+def main():
+    meow(1)
+
+#Define meow
+def meow(n):
     n=int(input("How many meows? "))
-    if n>0:
-        break
-    
-for _ in range(n):
-    print("Meow!")
+    while True:
+        if n>0:
+            break
+        
+    for _ in range(n):
+        print("Meow")
+
+main()
