17c46503eae22af57f2c26bdcdeb6f20447cc16b
Ben <bmwangi676@gmail.com>
2025-06-11 13:28:02 +0300

    added houses

--- hogwarts.py
+++ hogwarts.py
@@ -5,7 +5,5 @@ students ={
     "Ron":"Gryffindor",
     "Draco":"Slytherin"
 }
-print(students["Hermione"])
-print(students["Harry"])
-print(students["Ron"])
-print(students["Draco"])
+for student in students:
+    print(student,students[student], sep="-")
