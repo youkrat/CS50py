20d32e37cc3ab4c4c0e9c35f97973dc6483ab50e
Ben <bmwangi676@gmail.com>
2025-06-11 13:11:30 +0300

    Added ranking system

--- hogwarts.py
+++ hogwarts.py
@@ -1,5 +1,6 @@
 #Defining students in a list
 students = ["Hermione", "Harry","Ron"]
 
-for student in students:
-    print(student)
+for i in range(len(students)):
+    print(i+1, students[i])
+    
