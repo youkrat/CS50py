48558403761d59287bf91f8d1192e4ed203350d7
Ben <bmwangi676@gmail.com>
2025-06-13 12:37:59 +0300

    Added user input

--- mario.py
+++ mario.py
@@ -1,9 +1,9 @@
 def main():
-  print_square(4)
+  size=int(input("Enter size "))
+  print_square(size)
   
 def print_square(size):
-  size=int(input("Enter the side length "))
-  #For each row in square
+
   
   for i in range (size):
     #For each brick in row
