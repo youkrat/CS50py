e045f1e64b6f5bb6a2d7ac06dd61180e0979ced6
Ben <bmwangi676@gmail.com>
2025-06-13 12:06:47 +0300

    Added squares

--- mario.py
+++ mario.py
@@ -1,7 +1,16 @@
 def main():
-    print_row(4)
-
-def print_row(length):
-    print("?"*length, sep="", end="\n")
+  print_square(4)
+  
+def print_square(size):
+  size=int(input("Enter the side length "))
+  #For each row in square
+  
+  for i in range (size):
+    #For each brick in row
     
+      for j in range (size):
+        #Print brick 
+        
+        print("#", end="")
+      print()
 main()
