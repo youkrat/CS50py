fadbbfbfad65b8f1353b37551c68d2dcb1ec985f
Ben <bmwangi676@gmail.com>
2025-06-16 17:13:03 +0300

    Added even and odd function

--- generate.py
+++ generate.py
@@ -1,6 +1,11 @@
-#Imports the choice module from the 'random' library
-from random import choice 
+#Imports the randint module from random
+from random import randint
 
-#removes the need of random.choice
-coin = choice(["heads", "tails"])
-print(coin)  
+#Gets a random number from 1-10
+#Tests whether the number is even or odd and 
+#prints heads for odd and tails for even
+number = randint(1,10)
+if number%2 != 0:
+    print(f"{number} Heads")
+else:
+    print(f"{number} Tails")
