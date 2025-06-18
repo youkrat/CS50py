857295f6f4ac3ed69c7e771627df0fe4073d6e35
Ben <bmwangi676@gmail.com>
2025-06-18 13:36:11 +0300

    Added Slices

--- name.py
+++ name.py
@@ -3,8 +3,5 @@ import sys
 #Check for errors
 if len(sys.argv)< 3:
     sys.exit("Too few arguments")
-elif len(sys.argv)>3:
-    sys.exit("Too many arguments")
-
-#print name tags
-print("Hello, my name is", sys.argv[1], sys.argv[2])
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)
