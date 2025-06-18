4058b967b00fc3b9d6d509152673157495c34554
Ben <bmwangi676@gmail.com>
2025-06-18 13:37:03 +0300

    Added comments

--- name.py
+++ name.py
@@ -3,5 +3,6 @@ import sys
 #Check for errors
 if len(sys.argv)< 3:
     sys.exit("Too few arguments")
+#Skips the first element of the list(name.py)
 for arg in sys.argv[1:]:
     print("Hello, my name is", arg)
