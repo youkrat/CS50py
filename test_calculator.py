f34b9732e15a84b5d7cfc5cb153a850ce8607ffa
Ben <bmwangi676@gmail.com>
2025-06-19 14:36:08 +0300

    Used Assert for testing(not ideal)

--- test_calculator.py
+++ test_calculator.py
@@ -3,9 +3,13 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4")
-    if square(3)!=9:
+    try:
+        assert square(-2)==4
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert square(3)==9
+    except AssertionError:
         print("3 squared was not 9")
         
 if __name__ =="__main__":
