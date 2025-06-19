02687fdcd1a64956ed29729abce668c89027ccc0
Ben <bmwangi676@gmail.com>
2025-06-19 14:13:24 +0300

    Added Error handling

--- calculator.py
+++ calculator.py
@@ -1,9 +1,37 @@
 def main():
-    x= float(input("What is x?"))
-    print("x squared is", square(x))
+    print("MODEL CALCULATOR")
+    print("Select a function:")
+    print("1. Square a number")
+    print("2. Raise a number to a power")
+    while True:
+        try:
+            function=int(input("What function? "))
+            
+            if function==1:
+                x= get_float("What's x:")
+                print(f"{x} squared is, {square(x)}")
+                
+            elif function ==2:
+                x=get_float("Enter base: ")
+                y=get_float("Enter exponent: ")
+                print(power(x,y))
+            else:
+                print("Unavailable, try again later")
+        except ValueError:
+            print("Please enter a valid number.")
+        return function
+        
+        
 
 def square(n):
     return n * n
     
-    
+def power(a, b):
+    return a**b
+def get_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Please enter a valid number")
 main()
