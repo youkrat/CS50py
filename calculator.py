45fe8dee9406c50e1d6a28f57a63c187b5cd49fe
Ben <bmwangi676@gmail.com>
2025-06-19 14:56:16 +0300

    Solved UnBound local Error

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
 def main():
-    print("MODEL CALCULATOR")
-    print("Select a function:")
-    print("1. Square a number")
-    print("2. Raise a number to a power")
+    
     while True:
         try:
+            print("MODEL CALCULATOR")
+            print("Select a function:")
+            print("1. Square a number")
+            print("2. Raise a number to a power")
             function=int(input("What function? "))
             
             if function==1:
@@ -19,7 +20,7 @@ def main():
                 print("Unavailable, try again later")
         except ValueError:
             print("Please enter a valid number.")
-        return function
+        return
         
         
 
