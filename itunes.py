4628511af1f51848029d1e25a4fb3c570436b1af
Ben <bmwangi676@gmail.com>
2025-06-19 12:35:24 +0300

    Prints artist trackName

--- itunes.py
+++ itunes.py
@@ -5,5 +5,12 @@ import json
 if len(sys.argv)!=2:
     sys.exit()
     
-response=requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+#Making a HTTP request to itunes using python
+response=requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+sys.argv[1])
+
+#Grabs the json object I care about 
+o=response.json()
+
+#Prints the results and the key value in trackName
+for result in o["results"]:
+    print(result["trackName"])
