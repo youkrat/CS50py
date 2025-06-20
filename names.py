81cdf92586efc4be7be8af5371feeff4e29aa125
Ben <bmwangi676@gmail.com>
2025-06-21 00:25:17 +0300

    Writes name on new line

--- names.py
+++ names.py
@@ -1,7 +1,5 @@
-names=[]
+name = input("What's your name? ")
 
-for _ in range(3):
-    names.append(input("What's your name? "))
-    
-for name in sorted(names): 
-    print(f"hello, {name}")
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close()
