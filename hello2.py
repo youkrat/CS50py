27ebb00272f3111ca3cdcbdcae1ce5cf88c2e6d4
Ben <bmwangi676@gmail.com>
2025-06-20 23:04:11 +0300

    Changed functionality and removed comments

--- hello2.py
+++ hello2.py
@@ -1,15 +1,12 @@
-#Defining the main part of the code 
 def main():
-#Removing whitespace and capitalizing the user's name
+    
     hello()
-    name = input("What's your name? ",).strip().title()
-    first, second, surname = name.split()
-#Calling the function hello, and saying hello to the user
+    name = input("What's your name? ").strip().title()
     hello(name)
+ 
 
-#Defining the Hello function and the parameters
-#(to="world") sets the default value for the parameter to 
-def hello(to="world"):
-    print("Hello,", to )
-#Calling the main function
-main()
+def hello(to ="World"):
+    print("Hello,",to)
+    
+if __name__=="__main__":
+    main()
