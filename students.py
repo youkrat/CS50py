7d559ee528b1598b73ae46216b1325f9c4653bcb
Ben <bmwangi676@gmail.com>
2025-06-25 01:13:08 +0300

    Used key=lambda for sorting

--- students.py
+++ students.py
@@ -5,9 +5,7 @@ with open("students.csv") as file:
         name, house=line.rstrip().split(",")
         student ={"name":name, "house":house}
         students.append(student)
-
-def get_name(student):
-    return student["name"]
-
-for student in sorted(students, key=get_name):
+        
+#lambda creates an anonymous function that takes a value lambda "X"
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
