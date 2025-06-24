45f5e9e232263530ca17373cbca96f01f961f93b
Ben <bmwangi676@gmail.com>
2025-06-24 13:08:07 +0300

    Sorts from Z to A

--- read_names.py
+++ read_names.py
@@ -1,3 +1,8 @@
+names = []
+
 with open("names.txt") as file:
-    for line in sorted(file):
-       print(f"Hello, {line.rstrip()}")
+    for line in file:
+        names.append(line.rstrip())
+        
+for name in sorted(names, reverse=True):
+    print(f"Hello, {name}")
