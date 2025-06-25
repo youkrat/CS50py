e384fa90ffa3eb449f819cf6863423be0e7ddd3b
Ben <bmwangi676@gmail.com>
2025-06-26 01:48:36 +0300

    Changed values in csvfile and read function

--- students.py
+++ students.py
@@ -3,10 +3,12 @@ import csv
 students = []
 
 with open("students.csv") as file:
+
+#csv.DictReader is better since it assigns names to the columns 
     reader =csv.DictReader(file)
     for row in reader:
-        students.append({"name": row["name"], "home":row["home"]})
+        students.append({"name": row["name"], "home":row["home"], "house":row["house"]})
         
 #lambda creates an anonymous function that takes a value lambda "X"
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    print(f"{student['name']} is from {student['home']} in house {student['house']}")
