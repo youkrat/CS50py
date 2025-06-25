3a8e45d10373ead6832724e0f2362e0a2e1a12ad
Ben <bmwangi676@gmail.com>
2025-06-26 01:20:43 +0300

    Imported the csv function csv.reader

--- students.py
+++ students.py
@@ -1,11 +1,12 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house=line.rstrip().split(",")
-        student ={"name":name, "house":house}
-        students.append(student)
+    reader =csv.reader(file)
+    for row in reader:
+        students.append({"name": row[0], "home":row[1]})
         
 #lambda creates an anonymous function that takes a value lambda "X"
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is from {student['home']}")
