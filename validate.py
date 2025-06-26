7586e91b0cda3d14d167c291e415bd676c24bcf2
Ben <bmwangi676@gmail.com>
2025-06-26 22:44:55 +0300

    Checks for .edu emails

--- validate.py
+++ validate.py
@@ -1,6 +1,8 @@
 email = input("What's your email? ").strip()
 
-if "@" in email and "." in email:
+username, domain = email.split("@")
+
+if username and domain.endswith(".edu"):
     print("Valid")
 else:
     print("Invalid")
