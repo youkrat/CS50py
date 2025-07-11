432d42ddd21f0e27d10891c90bc89fee7721e64d
Ben <bmwangi676@gmail.com>
2025-07-12 01:19:56 +0300

    Added getters and setters to verify house

--- scholars.py
+++ scholars.py
@@ -1,46 +1,41 @@
 #Creates a class named Student
 class Student:
     #Added instance variables (attributes) to the object student
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        if not patronus:
-            raise ValueError("Missing patronus")
         self.name = name
         self.house =  house
-        self.patronus = patronus
         
         
     def __str__(self):
-        return f"{self.name} from {self.house} with the {self.patronus} patronus"
+        return f"{self.name} from {self.house}"
     
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "🐎"
-            case "Otter":
-                return "🦦"
-            case "Jack Russel Terrier":
-                return "🦮"
-            case _:
-                return "🎆"
+    #Getter, and accompanying assignment syntax
+    @property
+    def house(self):
+        return self._house
+    
+    #Setter and accompanying assignment syntax
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    #Constructor call. Constructs a student object 
-    return Student(name, house, patronus)
+    #Constructor call(Constructs a student object from the student class
+    #Equally achievable by student = student(name, house) return student
+    return Student(name, house)
      
 
 def main():
     student = get_student()
     if student.name == "Padma":
         student.house = "Ravenclaw"
-    print("Expecto patronum")
-    print(student.charm())
+    print(student)
     
 if __name__ =="__main__":
     main()
