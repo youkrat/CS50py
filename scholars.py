a9667db37b792b4e17e397cb05443dcfcd6f0de1
Ben <bmwangi676@gmail.com>
2025-07-11 23:28:23 +0300

    Initialized student obect as a str

--- scholars.py
+++ scholars.py
@@ -8,7 +8,10 @@ class Student:
             raise ValueError("Invalid house")
         self.name = name
         self.house =  house
-
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+        
 
 def get_student():
     name = input("Name: ")
@@ -21,7 +24,7 @@ def main():
     student = get_student()
     if student.name == "Padma":
         student.house = "Ravenclaw"
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 if __name__ =="__main__":
     main()
