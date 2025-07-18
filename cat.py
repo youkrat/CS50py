19650b79c5ca7f95bfc0695cfddb9b0b459d981d
Ben <bmwangi676@gmail.com>
2025-07-18 23:32:35 +0300

    Fixed the data type error

--- cat.py
+++ cat.py
@@ -1,11 +1,10 @@
 #Added a type hint to let the program know that n should be an int
-#-> None hints that the return value of meow should be None 
-def meow(n: int) -> None:
-    for _ in range(n):
-        print("Meow")
+#-> str hints that the return value of meow should be str
+def meow(n: int) -> str:
+    return "meow\n" * n
 
 number: int = int(input("Number: "))
 meows: str = meow(number)
 #Brings an error whereby print(meows) prints None as the return value of meows()
 #is none. mypy cat.py helps catch this error if the default return value is set to none 
-print(meows)
+print(meows, end ="")
